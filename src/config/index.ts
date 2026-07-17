08947ff791dd9a8006850081742563015fa8b822
mdrakibulhasanparvej <mdrakibulhasanparvej@gmail.com>
2026-07-17 22:33:01 +0600

    config file updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,3 +13,5 @@ export default {
   jwt_access_expiration: process.env.JWT_ACCESS_EXPIRATION!,
   jwt_refresh_expiration: process.env.JWT_REFRESH_EXPIRATION!,
 };
+
+// all the environment variables are loaded from the .env file and exported as a default object for use in other parts of the application. The exclamation marks after some of the environment variables indicate that these values are expected to be defined and will throw an error if they are not.
